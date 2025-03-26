522938405ecfd7775af3ab19a18337eb48f9bf29
Ak-Rajak <atulkumar.coderguys@gmail.com>
2025-03-27 00:52:45 +0530

    Setuped the routing the for both public and protected routes

--- ai-mock-interview-platform/src/App.tsx
+++ ai-mock-interview-platform/src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router , Routes , Route } from "react-router-dom"
-
+import  PublicLayouts  from "@/layouts/PublicLayouts"
+import HomePage from "@/Routes/Home"
 const App = () => {
   return (
     <Router>
       <Routes>
         {/* public routes */}
-        
+        <Route element={<PublicLayouts />}>
+          <Route index element={<HomePage/>} />
+        </Route>
         {/* protected routes */}
       </Routes>
     </Router>
--- ai-mock-interview-platform/src/layouts/PublicLayouts.tsx
+++ ai-mock-interview-platform/src/layouts/PublicLayouts.tsx
@@ -1,7 +1,16 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import { Outlet } from "react-router-dom";
+
 export default function PublicLayouts() {
   return (
     <div className="w-full">
         {/* handle user data as its is authenticated or not so that if he is not the we put it into firebase  */}
+
+        <Header/> 
+        {/* Outlet will render the route in like Homepage */}
+        <Outlet/> 
+        <Footer/>
     </div>
   )
 }

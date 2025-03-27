9e023b7fd177a0404389285dd2af3510764e05ef
Ak-Rajak <atulkumar.coderguys@gmail.com>
2025-03-28 01:00:45 +0530

    Completed the mainLaoyout page

--- ai-mock-interview-platform/src/layouts/MainLayouts.tsx
+++ ai-mock-interview-platform/src/layouts/MainLayouts.tsx
@@ -1,3 +1,4 @@
+import Containers from "@/components/Containers"
 import Footer from "@/components/Footer"
 import Header from "@/components/Header"
 import { Outlet } from "react-router"
@@ -6,9 +7,13 @@ const MainLayouts = () => {
   return (
     <div className="flex flex-col h-screen">
         <Header/>
-
-        <Outlet/>
-
+        {/* Wrapping the Conatiner Components in the outlet */}
+        <Containers className="flex-grow">
+            <main className="flex-grow">
+                <Outlet/>
+            </main> 
+        </Containers>
+    
         <Footer/>
     </div>
   )

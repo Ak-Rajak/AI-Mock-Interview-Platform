981a4bd1d37f7cef070e934008275d37a8eaec7c
Ak-Rajak <atulkumar.coderguys@gmail.com>
2025-03-29 01:55:32 +0530

    Also setting up the config file for the firebase

--- ai-mock-interview-platform/src/config/firebase-config.ts
+++ ai-mock-interview-platform/src/config/firebase-config.ts
@@ -1,5 +1,5 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
-import getFirestore from "firebase/compat/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,

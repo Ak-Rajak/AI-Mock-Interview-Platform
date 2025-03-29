9959cd6ebf8ddd571b7594291cf2535403c97bbd
Ak-Rajak <atulkumar.coderguys@gmail.com>
2025-03-29 20:53:52 +0530

    Working on auth handler function  storeUserdata for firebase

--- ai-mock-interview-platform/src/handlers/auth-handler.tsx
+++ ai-mock-interview-platform/src/handlers/auth-handler.tsx
@@ -1,18 +1,40 @@
 import { useAuth, useUser } from "@clerk/clerk-react";
-import { useState } from "react";
+import { Loader } from "lucide-react";
+import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router";
 
-
 export const AuthHandler = () => {
-
-  // parameters to used in the firebase authentication inside handler 
+  // parameters to used in the firebase authentication inside handler
   const { isSignedIn } = useAuth();
-  const {user} = useUser();
+  const { user } = useUser();
 
   const pathname = useLocation().pathname;
-  const navigate = useNavigate(); 
-  
-  const [loading , setLoading]  = useState(false);
+  const navigate = useNavigate();
 
-  return null
-}
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const storeUserData = async () => {
+      // check if user is signed in or not , then store user data into firebase
+      if (isSignedIn && user) {
+        setLoading(true);
+        try {
+          const userSnap = await getDoc(doc(db, "users", user.id));
+          if (!userSnap.exists()) {
+          }
+        } catch (error) {
+          console.log("Error on storing the user data : ", error);
+        } finally {
+          setLoading(false);
+        }
+      }
+    };
+  }, [isSignedIn, user, pathname, navigate]);
+
+  // Loading
+  if (loading) {
+    return <Loader />;
+  }
+
+  return null;
+};

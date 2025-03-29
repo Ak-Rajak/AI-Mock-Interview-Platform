bbf0e0454b19df4e20a92723ffafcf088cd6fcbb
Ak-Rajak <atulkumar.coderguys@gmail.com>
2025-03-29 20:26:45 +0530

    importing the necessary parameters in the handler

--- ai-mock-interview-platform/src/handlers/auth-handler.tsx
+++ ai-mock-interview-platform/src/handlers/auth-handler.tsx
@@ -1,7 +1,18 @@
+import { useAuth, useUser } from "@clerk/clerk-react";
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router";
 
 
 export const AuthHandler = () => {
-  return (
-    null
-  )
+
+  // parameters to used in the firebase authentication inside handler 
+  const { isSignedIn } = useAuth();
+  const {user} = useUser();
+
+  const pathname = useLocation().pathname;
+  const navigate = useNavigate(); 
+  
+  const [loading , setLoading]  = useState(false);
+
+  return null
 }

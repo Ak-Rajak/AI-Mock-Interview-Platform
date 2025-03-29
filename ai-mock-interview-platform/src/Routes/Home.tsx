6818da75280593d46add9feced21086788ceaeeb
Ak-Rajak <atulkumar.coderguys@gmail.com>
2025-03-30 00:00:35 +0530

    get started with with the home page

--- ai-mock-interview-platform/src/Routes/Home.tsx
+++ ai-mock-interview-platform/src/Routes/Home.tsx
@@ -1,6 +1,24 @@
+import Containers from "@/components/Containers"
+
+
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className="flex-col w-full pb-24">
+      <Containers>
+        <div className="my-8">
+          <h2 className="text-3xl text-center md:text-left md:text-6xl">
+            <span className=" text-outline font-extrabold md:text-8xl">
+              AI Superpower
+            </span>
+            <span className="text-gray-500 font-extrabold">
+              - A better way to
+            </span>
+            <br /> 
+            improve your interview chances and skills
+          </h2>
+        </div>
+      </Containers>
+    </div>
   )
 }
 

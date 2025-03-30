b56af2bc5034c76b48ab2e594ac2a7ee832714a4
Ak-Rajak <atulkumar.coderguys@gmail.com>
2025-03-31 00:50:05 +0530

    setup the dashboard separator and headings

--- ai-mock-interview-platform/src/Routes/Dashboard.tsx
+++ ai-mock-interview-platform/src/Routes/Dashboard.tsx
@@ -1,10 +1,28 @@
+import { Headings } from "@/components/Headings";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Plus } from "lucide-react";
+import { Link } from "react-router";
+
 export const Dashboard = () => {
   return (
+    <>
     <div className="flex w-full items-center justify-between">
-        {/* Heading section */}
-        
-
-        {/* content section */}
+      {/* Heading section */}
+      <div className="flex w-full items-center justify-between">
+        <Headings
+          title="Dashboard"
+          description="Create and start you AI Mock interview"
+        />
+        <Link to={"/generate/create"}>
+            <Button size={"sm"}>
+                <Plus/> Add New
+            </Button>
+        </Link>
+      </div>
     </div>
+    <Separator className="my-8"/>
+    {/* content section */}
+    </>
   );
 };

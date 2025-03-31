ec2cd4c8268a46ad3c1ea7d0060535a62a6554bd
Ak-Rajak <atulkumar.coderguys@gmail.com>
2025-03-31 23:44:47 +0530

    building the form , input and textarea

--- ai-mock-interview-platform/src/components/FormMockInterview.tsx
+++ ai-mock-interview-platform/src/components/FormMockInterview.tsx
@@ -1,11 +1,44 @@
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { Interview } from "@/types";
+import { CustomBreadCrum } from "./CustomBreadCrum";
+import { BreadcrumbPage } from "./ui/breadcrumb";
 
 interface FormMockInterviewProps {
-    initialData: Interview | null;
+  initialData: Interview | null;
 }
 
-export const FormMockInterview = ({initialData} : FormMockInterviewProps ) => {
+// This is the schema for the form validsation
+const formSchema = z.object({
+  position: z
+    .string()
+    .min(1, "Position is required")
+    .max(100, "Position must be 100 characters or less"),
+  description: z.string().min(10, "Description is required"),
+  experience: z.coerce
+    .number()
+    .min(0, "Experience cannot be empty or negative"),
+  techStack: z.string().min(1, "Tech stack must be at least a character"),
+});
+
+type FormData = z.infer<typeof formSchema>;
+
+export const FormMockInterview = ({ initialData }: FormMockInterviewProps) => {
+    
+    // This is the schema for the form validsation
+    const form = useForm<FormData>({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData || {}
+    });
+
+    
   return (
-    <div>FormMockInterview</div>
-  )
-}
+    <div className="w-full flex-col space-y-4">
+      <CustomBreadCrum
+        breadCrumbPage={BreadcrumbPage}
+        breadCrumbItems={[{ label: "Mock Interview", link: "/generate" }]}
+      />
+    </div>
+  );
+};

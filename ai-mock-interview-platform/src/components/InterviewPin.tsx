e0d7b4aabdc6970f79ff8dd3670a23b2d7708863
Ak-Rajak <atulkumar.coderguys@gmail.com>
2025-04-06 00:28:09 +0530

    Completed the interview pin componenets

--- ai-mock-interview-platform/src/components/InterviewPin.tsx
+++ ai-mock-interview-platform/src/components/InterviewPin.tsx
@@ -12,6 +12,8 @@ import {
 import { useNavigate } from "react-router";
 import { Badge } from "./ui/badge";
 import { cn } from "@/lib/utils";
+import { TooltipButton } from "./TooltipButton";
+import { Eye, Newspaper, Pencil, Sparkles } from "lucide-react";
 
 interface InterviewPinProps {
   interview: Interview;
@@ -32,28 +34,74 @@ export const InterviewPin = ({
       <CardTitle className="text-lg">{interview?.position}</CardTitle>
       <CardDescription>{interview?.description}</CardDescription>
       <div className="w-full flex items-center gap-2 flex-wrap">
-        {interview.techStack.split(",").map((word , index) => (
-          <Badge key={index}
-          variant={"outline"}
-          className="text-xs text-muted-foreground hover:border-emerald-400 hover:bg-emerald-50 hover:text-emerald-900">
+        {interview.techStack.split(",").map((word, index) => (
+          <Badge
+            key={index}
+            variant={"outline"}
+            className="text-xs text-muted-foreground hover:border-emerald-400 hover:bg-emerald-50 hover:text-emerald-900"
+          >
             {word}
           </Badge>
         ))}
       </div>
-      <CardFooter className={cn(
-        "w-full flex items-center p-0",
-        onMockPage ? "justify-end" : "justify-between"
-      )}>
+      <CardFooter
+        className={cn(
+          "w-full flex items-center p-0",
+          onMockPage ? "justify-end" : "justify-between"
+        )}
+      >
         <p className="text-[12px] text-muted-foreground truncate whitespace-nowrap ">
           {`${new Date(interview.createdAt.toDate()).toLocaleDateString(
-            "en-US" , 
-            {dateStyle: "long"}
-          )} - ${new Date(interview.createdAt.toDate()).toLocaleTimeString("en-US" , {timeStyle:"short"})}`}
+            "en-US",
+            { dateStyle: "long" }
+          )} - ${new Date(interview.createdAt.toDate()).toLocaleTimeString(
+            "en-US",
+            { timeStyle: "short" }
+          )}`}
         </p>
 
-        {/* Action button */}
+        {/* Action buttons */}
         {!onMockPage && (
-          <div className="flex items-center justify-center"></div>
+          <div className="flex items-center justify-center">
+            <TooltipButton
+              content="View"
+              buttonVariant={"ghost"}
+              icon={<Eye />}
+              onClick={() => {
+                navigate(`/generate/${interview.id}`, { replace: true });
+              }}
+              loading={false}
+              buttonClassName="hover:text-sky-500"
+              disabled={false}
+            />
+            <TooltipButton
+              content={"Edit"}
+              buttonVariant={"ghost"}
+              icon={<Newspaper />}
+              onClick={() => {
+                navigate(`/generate/feedback/${interview.id}`, {
+                  replace: true,
+                });
+              }}
+              loading={false}
+              buttonClassName="hover:text-yellow-500"
+              disabled={false}
+            />
+
+            <TooltipButton
+              content="Start"
+              buttonVariant={"ghost"}
+              icon={<Sparkles />}
+              onClick={() => {
+                navigate(`/generate/interview/${interview.id}`, {
+                  replace: true,
+                });
+              }}
+              loading={false}
+              buttonClassName="hover:text-sky-500"
+              disabled={false}
+            />
+          </div>
         )}
       </CardFooter>
     </Card>

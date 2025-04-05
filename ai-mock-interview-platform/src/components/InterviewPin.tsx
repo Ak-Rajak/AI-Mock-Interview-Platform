d4cf180cf417ad2ffdaafbd6d3761f448ff35f0f
Ak-Rajak <atulkumar.coderguys@gmail.com>
2025-04-05 20:56:43 +0530

    Design the Pin components to display the interview

--- ai-mock-interview-platform/src/components/InterviewPin.tsx
+++ ai-mock-interview-platform/src/components/InterviewPin.tsx
@@ -1,11 +1,48 @@
 import { Interview } from "@/types";
+import { useAuth } from "@clerk/clerk-react";
+import { useState } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useNavigate } from "react-router";
+import { Badge } from "./ui/badge";
+import { cn } from "@/lib/utils";
 
-interface InterviewPinProps{
-    interview : Interview;
+interface InterviewPinProps {
+  interview: Interview;
+  // Action should not display on onMockPage
+  onMockPage?: boolean;
 }
 
-export const InterviewPin = ({interview} : InterviewPinProps) => {
+export const InterviewPin = ({
+  interview,
+  onMockPage = false,
+}: InterviewPinProps) => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const { userId } = useAuth();
+
   return (
-    <div>InterviewPin</div>
-  )
-}
+    <Card className="p-4 rounded-md shadow-none hover:shadow-md shadow-gray-100 cursor-pointer transition-all space-y-4">
+      <CardTitle className="text-lg">{interview?.position}</CardTitle>
+      <CardDescription>{interview?.description}</CardDescription>
+      <div className="w-full flex items-center gap-2 flex-wrap">
+        {interview.techStack.split(",").map((word , index) => (
+          <Badge key={index}
+          variant={"outline"}
+          className="text-xs text-muted-foreground hover:border-emerald-400 hover:bg-emerald-50 hover:text-emerald-900">
+            {word}
+          </Badge>
+        ))}
+      </div>
+      <CardFooter className={cn(
+        "w-full flex items-center p-0"
+      )}></CardFooter>
+    </Card>
+  );
+};

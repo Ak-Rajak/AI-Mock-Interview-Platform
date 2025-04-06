c041bf36b60d4b14367425095f36a5ebecd7a000
Ak-Rajak <atulkumar.coderguys@gmail.com>
2025-04-07 00:11:44 +0530

    Added the speech to text hook

--- ai-mock-interview-platform/src/components/RecordAnswer.tsx
+++ ai-mock-interview-platform/src/components/RecordAnswer.tsx
@@ -1,7 +1,27 @@
+import useSpeechToText from 'react-hook-speech-to-text';
 
 
-export const RecordAnswer = () => {
-  return (
-    <div>RecordAnswer</div>
-  )
+interface RecordAnswerProps {
+  question: { question: string; answer: string };
+  isWebCam: boolean;
+  setIsWebCam: (value: boolean) => void;
 }
+
+export const RecordAnswer = ({
+  question,
+  isWebCam,
+  setIsWebCam,
+}: RecordAnswerProps) => {
+    const {
+        error,
+        interimResult,
+        isRecording,
+        results,
+        startSpeechToText,
+        stopSpeechToText,
+      } = useSpeechToText({
+        continuous: true,
+        useLegacyResults: false
+      });
+    return <div>RecordAnswer</div>
+};

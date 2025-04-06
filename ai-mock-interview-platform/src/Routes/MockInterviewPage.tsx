69399e0da2f7053564b1426759637ee39ad483fc
Ak-Rajak <atulkumar.coderguys@gmail.com>
2025-04-06 15:06:17 +0530

    Started working on mockinterviewpage

--- ai-mock-interview-platform/src/Routes/MockInterviewPage.tsx
+++ ai-mock-interview-platform/src/Routes/MockInterviewPage.tsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import { Interview } from "@/types";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
+import LoaderPage from "./Loaderpage";
+
 
 export const MockInterviewPage = () => {
+    const { interviewId } = useParams<{ interviewId: string }>();
+      const [interview, setInterview] = useState<Interview | null>(null);
+      const [isLoading, setIsLoading] = useState(false);
+      const navigate = useNavigate();
+
+      useEffect(() => {} ,[
+        interviewId,
+        navigate,
+      ]);
+
+      if(isLoading) {
+        return  <LoaderPage className="w-full h-[70vh]" />;
+      }
+
+      if(! interviewId){
+        navigate("/generate", { replace: true });
+      }
+
   return (
     <div>MockInterviewPage</div>
   )

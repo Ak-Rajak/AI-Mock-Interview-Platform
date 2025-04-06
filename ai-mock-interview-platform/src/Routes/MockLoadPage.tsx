a90c0be6b9dfe20503c68cadee231e70c6f500fa
Ak-Rajak <atulkumar.coderguys@gmail.com>
2025-04-06 11:02:12 +0530

    Added the alert for interview

--- ai-mock-interview-platform/src/Routes/MockLoadPage.tsx
+++ ai-mock-interview-platform/src/Routes/MockLoadPage.tsx
@@ -2,52 +2,95 @@ import { db } from "@/config/firebase-config";
 import { Interview } from "@/types";
 import { doc, getDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router"
+import { Link, useNavigate, useParams } from "react-router";
 import LoaderPage from "./Loaderpage";
-
+import { CustomBreadCrum } from "@/components/CustomBreadCrum";
+import { Button } from "@/components/ui/button";
+import { Lightbulb, Sparkle, Sparkles } from "lucide-react";
+import { InterviewPin } from "@/components/InterviewPin";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 export const MockLoadPage = () => {
-    // Necessary states 
-    const {interviewId} = useParams<{interviewId : string}>();
-    const [interview , setInterview] = useState<Interview | null>(null);
-    const [isLoading , setIsLoading] = useState(false);
-    const [isWebCamEnabled , setIsWebCamEnabled] = useState(false);
+  // Necessary states
+  const { interviewId } = useParams<{ interviewId: string }>();
+  const [interview, setInterview] = useState<Interview | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isWebCamEnabled, setIsWebCamEnabled] = useState(false);
 
-    // navigate hooks 
-    const navigate = useNavigate();
+  // navigate hooks
+  const navigate = useNavigate();
 
-    // fetch data from firebase for a particular inputs
-    useEffect(() => {
-        setIsLoading(true);
-        const fetchInterview = async () =>{
-            if (interviewId) {
-                try {
-                    const interviewDoc = await getDoc(doc(db, "interviews" , interviewId));
-                    if (interviewDoc.exists()) {
-                        setInterview({
-                            id: interviewDoc.id,
-                            ...interviewDoc.data(),
-                        } as Interview);
-                    }
-                } catch (error) {
-                    console.log(error);
-                } finally {
-                    setIsLoading(false);
-                }
-            }
+  // fetch data from firebase for a particular inputs
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchInterview = async () => {
+      if (interviewId) {
+        try {
+          const interviewDoc = await getDoc(doc(db, "interviews", interviewId));
+          if (interviewDoc.exists()) {
+            setInterview({
+              id: interviewDoc.id,
+              ...interviewDoc.data(),
+            } as Interview);
+          }
+        } catch (error) {
+          console.log(error);
+        } finally {
+          setIsLoading(false);
         }
+      }
+    };
 
-        fetchInterview();
-    } , [interviewId , navigate]);
+    fetchInterview();
+  }, [interviewId, navigate]);
 
-    if(isLoading) {
-        return <LoaderPage className="w-full h-[70vh]" />
-    }
+  if (isLoading) {
+    return <LoaderPage className="w-full h-[70vh]" />;
+  }
+
+  if (!interviewId) {
+    navigate("/generate", { replace: true });
+  }
+
+  if (!interview) {
+    navigate("/generate", { replace: true });
+  }
+
   return (
     <div className="flex flex-col w-full gap-8 py-5">
-        <div className="flex items-center justify-between w-full gap-2">
+      <div className="flex items-center justify-between w-full gap-2">
+        <CustomBreadCrum
+          breadCrumbPage={interview?.position || ""}
+          breadCrumbItems={[{ label: "Mock Interviews", link: "/generate" }]}
+        />
 
+        <Link to={`/generate/interview/${interviewId}/start`}>
+          <Button size={"sm"}>
+            Start <Sparkles />
+          </Button>
+        </Link>
+      </div>
+
+      {/* Conditional rendering that display interview pin */}
+      {interview && <InterviewPin interview={interview} onMockPage />}
+
+      {/* Alert component for shadcn */}
+      <Alert className="bg-yellow-100/50 border-yellow-200 p-4 rounded-lg flex items-start gap-3 -mt-3">
+        <Lightbulb className="h-5 w-5 text-yellow-600" />
+        <div>
+          <AlertTitle>Important Infomation</AlertTitle>
+          <AlertDescription className="text-sm text-yellow-700 mt-1">
+          Please enable your webcam and microphone to start the AI-generated
+            mock interview. The interview consists of five questions. You’ll
+            receive a personalized report based on your responses at the end.{" "}
+            <br />
+            <br />
+            <span className="font-medium">Note:</span> Your video is{" "}
+            <strong>never recorded</strong>. You can disable your webcam at any
+            time.
+          </AlertDescription>
         </div>
+      </Alert>
     </div>
-  )
-}
+  );
+};

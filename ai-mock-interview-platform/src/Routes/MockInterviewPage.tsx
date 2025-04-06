aa4ffab240874272dc8e784a43ef6f9920cc80c2
Ak-Rajak <atulkumar.coderguys@gmail.com>
2025-04-06 19:54:08 +0530

    Added the breadcrum components on interview

--- ai-mock-interview-platform/src/Routes/MockInterviewPage.tsx
+++ ai-mock-interview-platform/src/Routes/MockInterviewPage.tsx
@@ -2,28 +2,63 @@ import { Interview } from "@/types";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
 import LoaderPage from "./Loaderpage";
-
+import { CustomBreadCrum } from "@/components/CustomBreadCrum";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "@/config/firebase-config";
 
 export const MockInterviewPage = () => {
-    const { interviewId } = useParams<{ interviewId: string }>();
-      const [interview, setInterview] = useState<Interview | null>(null);
-      const [isLoading, setIsLoading] = useState(false);
-      const navigate = useNavigate();
+  const { interviewId } = useParams<{ interviewId: string }>();
+  const [interview, setInterview] = useState<Interview | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
-      useEffect(() => {} ,[
-        interviewId,
-        navigate,
-      ]);
-
-      if(isLoading) {
-        return  <LoaderPage className="w-full h-[70vh]" />;
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchInterview = async () => {
+      if (interviewId) {
+        try {
+          const interviewDoc = await getDoc(doc(db, "interviews", interviewId));
+          if (interviewDoc.exists()) {
+            setInterview({
+              id: interviewDoc.id,
+              ...interviewDoc.data(),
+            } as Interview);
+          }
+        } catch (error) {
+          console.log(error);
+        } finally {
+          setIsLoading(false);
+        }
       }
+    };
 
-      if(! interviewId){
-        navigate("/generate", { replace: true });
-      }
+    fetchInterview();
+  }, [interviewId, navigate]);
+
+  if (isLoading) {
+    return <LoaderPage className="w-full h-[70vh]" />;
+  }
+
+  if (!interviewId) {
+    navigate("/generate", { replace: true });
+  }
+
+  if (!interview) {
+    navigate("/generate", { replace: true });
+  }
 
   return (
-    <div>MockInterviewPage</div>
-  )
-}
+    <div className="flex flex-col w-full gap-8 py-5">
+      <CustomBreadCrum
+        breadCrumbPage="Start"
+        breadCrumbItems={[
+          { label: "Mock Interviews", link: "/generate" },
+          {
+            label: interview?.position || "",
+            link: `/generate/interview/${interview?.id}`,
+          }
+        ]}
+      />
+    </div>
+  );
+};

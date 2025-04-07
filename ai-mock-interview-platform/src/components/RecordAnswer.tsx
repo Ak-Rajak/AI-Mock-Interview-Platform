b38f167994a5b22bef9becf1d7f38227fe590b2f
Ak-Rajak <atulkumar.coderguys@gmail.com>
2025-04-07 20:33:41 +0530

    Created all the necessary states for the record answer components

--- ai-mock-interview-platform/src/components/RecordAnswer.tsx
+++ ai-mock-interview-platform/src/components/RecordAnswer.tsx
@@ -1,4 +1,7 @@
+import { useAuth } from "@clerk/clerk-react";
+import { useState } from "react";
 import useSpeechToText from "react-hook-speech-to-text";
+import { useParams } from "react-router";
 
 interface RecordAnswerProps {
   question: { question: string; answer: string };
@@ -22,7 +25,21 @@ export const RecordAnswer = ({
     continuous: true,
     useLegacyResults: false,
   });
+
+  // States to have manage the answer wrt to questions
+  const [userAnswer , setUserAnswer] = useState("");
+  const [isAIGenerating , setIsAIGenerating] = useState(false);
+  const [aiResult , setAiResult] = useState<AIResponse | null>(null);
+  const [open , setOpen] = useState(false)
+  const [loading , setLoading] = useState(false)
+
+  const {userId} = useAuth();
+  const {interviewId} = useParams();
+
+
+  
+
+
   return <div>RecordAnswer</div>;
 };
 
-// Contine tomorrow 

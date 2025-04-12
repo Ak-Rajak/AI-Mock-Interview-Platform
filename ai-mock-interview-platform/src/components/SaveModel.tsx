6dd9b5701c95a5acb6282be643c8678ecd76e88f
Ak-Rajak <atulkumar.coderguys@gmail.com>
2025-04-12 20:44:16 +0530

    Fix the error

--- ai-mock-interview-platform/src/components/SaveModel.tsx
+++ ai-mock-interview-platform/src/components/SaveModel.tsx
@@ -14,24 +14,26 @@ export const SaveModel = ({
   onConfirm,
   loading,
 }: SaveModelProps) => {
-  <Model
-    title="Are you sure ?"
-    description="This action cannot be undone by you can't edit or re-answer this question again!"
-    isOpen={isOpen}
-    onClose={onClose}
-  >
-    <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-      <Button disabled={loading} variant={"outline"} onClick={onClose}>
-        Cancel
-      </Button>
+  return (
+    <Model
+      title="Are you sure ?"
+      description="This action cannot be undone by you can't edit or re-answer this question again!"
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+        <Button disabled={loading} variant={"outline"} onClick={onClose}>
+          Cancel
+        </Button>
 
-      <Button
-        disabled={loading}
-        className="bg-emerald-600 hover:bg-emerald-800"
-        onClick={onConfirm}
-      >
-        Continue
-      </Button>
-    </div>
-  </Model>;
+        <Button
+          disabled={loading}
+          className="bg-emerald-600 hover:bg-emerald-800"
+          onClick={onConfirm}
+        >
+          Continue
+        </Button>
+      </div>
+    </Model>
+  );
 };

c5b9bfc18ae1b3d088752b528f520cc3cc5054ad
Ak-Rajak <atulkumar.coderguys@gmail.com>
2025-04-13 01:10:25 +0530

    setting the function for fetching the interview data

--- ai-mock-interview-platform/src/Routes/FeedBack.tsx
+++ ai-mock-interview-platform/src/Routes/FeedBack.tsx
@@ -1,18 +1,50 @@
-import { Interview, UserAnswer } from '@/types';
-import { useAuth } from '@clerk/clerk-react';
-import React, { useState } from 'react'
-import { useNavigate, useParams } from 'react-router';
+import { db } from "@/config/firebase-config";
+import { Interview, UserAnswer } from "@/types";
+import { useAuth } from "@clerk/clerk-react";
+import { doc, getDoc } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
 
 export const FeedBack = () => {
-    const { interviewId } = useParams<{InterviewId : string}>();
-    const [interview , setInterview] = useState<Interview | null>(null);
-    const [isLoading , setIsLoading] = useState(false);
-    const [feedbacks , setFeedbacks] = useState<UserAnswer[]>([]);
-    const [activeFeed , setActiveFeed] = useState("");
-    const {userId} = useAuth();
-    const navigate = useNavigate();
-    
-  return (
-    <div>FeedBack</div>
-  )
-}
+  const { interviewId } = useParams<{ InterviewId: string }>();
+  const [interview, setInterview] = useState<Interview | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [feedbacks, setFeedbacks] = useState<UserAnswer[]>([]);
+  const [activeFeed, setActiveFeed] = useState("");
+  const { userId } = useAuth();
+  const navigate = useNavigate();
+
+  if (!interviewId) {
+    navigate("/generate", { replace: true });
+  }
+
+  useEffect(() => {
+    if (!interviewId) {
+       
+      // Function for fetching the interview data 
+      const fetchInterview = async () => {
+        if (interviewId) {
+          try {
+            const interviewDoc = await getDoc(
+              doc(db, "interviews", interviewId)
+            );
+            if (interviewDoc.exists()) {
+              setInterview({
+                id: interviewDoc.id,
+                ...interviewDoc.data(),
+              } as Interview);
+            }
+          } catch (error) {
+            console.log(error);
+          } 
+        }
+      };
+    }
+
+    // Fetch the feedbacks for the interview
+
+
+  }, []);
+
+  return <div>FeedBack</div>;
+};

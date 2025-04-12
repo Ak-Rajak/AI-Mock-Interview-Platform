853eef4175f1d591fa114e83f62cd16941bbd69d
Ak-Rajak <atulkumar.coderguys@gmail.com>
2025-04-13 01:21:16 +0530

    Added the function for the fetching the interview feedback response

--- ai-mock-interview-platform/src/Routes/FeedBack.tsx
+++ ai-mock-interview-platform/src/Routes/FeedBack.tsx
@@ -1,9 +1,18 @@
 import { db } from "@/config/firebase-config";
 import { Interview, UserAnswer } from "@/types";
 import { useAuth } from "@clerk/clerk-react";
-import { doc, getDoc } from "firebase/firestore";
+import { Description } from "@radix-ui/react-dialog";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
+import { toast } from "sonner";
 
 export const FeedBack = () => {
   const { interviewId } = useParams<{ InterviewId: string }>();
@@ -20,8 +29,7 @@ export const FeedBack = () => {
 
   useEffect(() => {
     if (!interviewId) {
-       
-      // Function for fetching the interview data 
+      // Function for fetching the interview data
       const fetchInterview = async () => {
         if (interviewId) {
           try {
@@ -36,14 +44,40 @@ export const FeedBack = () => {
             }
           } catch (error) {
             console.log(error);
-          } 
+          }
         }
       };
+
+      // Fetch the feedbacks for the interview
+      const fetchFeedbacks = async () => {
+        setIsLoading(true);
+        try {
+          const querySnapRef = query(
+            collection(db, "userAnswer"),
+            where("mockIdRef", "==", interviewId),
+            where("userId", "==", userId)
+          );
+
+          const querySnap = await getDocs(querySnapRef);
+
+          const interviewData: UserAnswer[] = querySnap.docs.map((doc) => {
+            return { id: doc.id, ...doc.data() } as UserAnswer;
+          });
+
+          setFeedbacks(interviewData);
+        } catch (error) {
+          console.log(error);
+          toast("Error", {
+            description: "Something went wrong. Please try again later..",
+          });
+        } finally {
+          setIsLoading(false);
+        }
+      };
+
+      fetchInterview();
+      fetchFeedbacks();
     }
-
-    // Fetch the feedbacks for the interview
-
-
   }, []);
 
   return <div>FeedBack</div>;
